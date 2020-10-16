5e9567c942fca95f6047678d7e1beda1c4c45003
pramitha1166 <63492955+pramitha1166@users.noreply.github.com>
2020-10-17 02:32:48 +0530

    configuration

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./Header.css";
+
+function header() {
+    return (
+        <div className="header">
+            <div className="header_left">
+                <img
+                    src="https://www.seekpng.com/png/full/51-516623_facebook-transparent-background-facebook-round-logo-blue-circle.png"
+                    />
+            </div>
+            <div className="header_middle">
+
+            </div>
+            <div className="header_right">
+
+            </div>
+        </div>
+    )
+}
+
+export default header
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCqLm-Z3IPvwgpalIdDz5-yZiqA-vLZBdI",
+    authDomain: "facebook-clone-50a47.firebaseapp.com",
+    databaseURL: "https://facebook-clone-50a47.firebaseio.com",
+    projectId: "facebook-clone-50a47",
+    storageBucket: "facebook-clone-50a47.appspot.com",
+    messagingSenderId: "28243297770",
+    appId: "1:28243297770:web:2c6687780ec490e5bf1e93",
+    measurementId: "G-8PGRLT1Q74"
+  };
