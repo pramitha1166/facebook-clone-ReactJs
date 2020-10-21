d0fb2e7a8209c256761272d7f14644ae6b4214ae
pramitha1166 <63492955+pramitha1166@users.noreply.github.com>
2020-10-21 09:56:36 +0530

    finished header design

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Header from "./Header"
 function App() {
   return (
     <div className="app">
-      <h1>Lets build FB CLONE</h1>
-
       <Header />
 
       {/* App Body */}
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import "./Header.css";
+import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import {Avatar, IconButton} from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import ForumIcon from '@material-ui/icons/Forum';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 function header() {
     return (
@@ -8,12 +20,51 @@ function header() {
                 <img
                     src="https://www.seekpng.com/png/full/51-516623_facebook-transparent-background-facebook-round-logo-blue-circle.png"
                     />
+                <div className="header_input">
+                    <SearchIcon />
+                    <input type="text" placeholder="Search Facebook"/>
+                </div>
             </div>
-            <div className="header_middle">
-
+            <div className="header_center">
+                 <div class="header_option 
+                 header_option--active">
+                    <HomeIcon 
+                    fontSize="large"/>
+                 </div>
+                 <div class="header_option">
+                    <FlagIcon 
+                    fontSize="large"/>
+                 </div>
+                 <div class="header_option">
+                    <SubscriptionsIcon 
+                    fontSize="large"/>
+                 </div>
+                 <div class="header_option">
+                    <StorefrontIcon 
+                    fontSize="large"/>
+                 </div>
+                 <div class="header_option">
+                    <SupervisedUserCircleIcon 
+                    fontSize="large"/>
+                 </div>
             </div>
             <div className="header_right">
-
+                <div class="header_info">
+                    <Avatar />
+                    <h4>saaaf</h4>
+                </div>
+                <IconButton>
+                    <AddIcon />
+                </IconButton>
+                <IconButton>
+                    <ForumIcon />
+                </IconButton>  
+                <IconButton>
+                    <NotificationsActiveIcon />
+                </IconButton>  
+                <IconButton>
+                    <ExpandMoreIcon />
+                </IconButton>                      
             </div>
         </div>
     )
